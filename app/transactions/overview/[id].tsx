d9defd4cb692eb0d983ef77f4e34e1a83eb4dd1a
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-05-17 22:51:48 +0300

    feat: add payments over-view pages

---
+++ app/transactions/overview/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PaymentId = () => {
+  return (
+    <View>
+      <Text>PaymentId</Text>
+    </View>
+  )
+}
+
+export default PaymentId
